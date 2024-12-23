9594420fc37e541c835c24876f7a4bc24c54ff84
Arijit Das <143489977+arijitdas0211@users.noreply.github.com>
2024-12-23 10:21:16 +0530

    adding operators 1

---
+++ Operators.js
@@ -0,0 +1,22 @@
+// console.log(10+20);
+// console.log(2**3);//8
+// var a = 10;
+// console.log(a++);//Post Incrementation - 10 But internall a is 11
+// console.log(a);// a = 11 
+// console.log(++a)//Pre Incrementation - 12
+
+// == Checks only value not data type:
+console.log(10 == 10); //true
+console.log(10 == '10'); //true
+
+// === Checks Value and Data Type Both
+console.log(10 === '10');//false : Values are matching but not data type
+console.log(10 === 10);//true
+
+// logical && , || and !
+console.log(true && true);
+console.log(true || false);
+console.log(!false);
+
+// Ternary operators
+console.log((10 == '10') ? "Equal by value" : "Not equal by value");
--- VariableDeclaration.js
+++ VariableDeclaration.js
@@ -1,1 +1,7 @@
-console.log('Hello from Node Js');
+//var, let and const
+
+{
+    var a = 10;
+    console.log('Accessing a inside the block', a);
+}
+console.log('Access a outside the block', a);
