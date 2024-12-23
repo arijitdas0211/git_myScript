01b6da1df157253d54bffd1e0175ec02daf5ab8a
Arijit Das <143489977+arijitdas0211@users.noreply.github.com>
2024-12-23 10:43:02 +0530

    adding Functions 1

---
+++ Functions.js
@@ -0,0 +1,22 @@
+// 4 types of functions:
+
+// Functions without input without return
+function add() {
+    var a = 20;
+    var b = 10;
+    console.log(a+b);
+}
+add();
+
+// Funtions with input without return
+function sub(x, y) {
+    console.log(x-y);
+    
+}
+sub(200, 100);
+
+// Funtions without input with return
+function prod() {}
+
+// Funtions with input with return
+
