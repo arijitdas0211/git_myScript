119c424666bdaf84fd3265f82a4a36c52d8784fd
Arijit Das <143489977+arijitdas0211@users.noreply.github.com>
2024-12-24 11:00:04 +0530

    JSON data and data types

---
+++ DataTypes.js
@@ -0,0 +1,24 @@
+// PRIMITTIVE DATA TYPES
+// Number Data Types
+var a = 902340178.56872;
+console.log(a, typeof a);
+
+// String
+var b = 'K';
+console.log(b, typeof b);
+
+// String
+var c = 'KodNest';
+console.log(c, typeof c);
+
+// Boolean
+var d = true;
+console.log(d, typeof d);
+
+// Undefined
+let e;
+console.log(e);
+
+// Null
+let f = null;
+console.log(f);
