22742d57f2c7ffd679ea622e639004107ae1cb0c
Arijit Das <143489977+arijitdas0211@users.noreply.github.com>
2024-12-26 10:53:53 +0530

    Array Obj 1

--- ArrayObject.js
+++ ArrayObject.js
@@ -1,7 +1,13 @@
 var arr1 = [10, 20, 30, 40];
 
+var arr2 = [10, 50.45, true, 'Kod', { name: 'Priya', age: 45 }, () => { console.log('Function') }, [100, 200]];
+
 for (let i = 0; i < arr1.length; i++) {
     console.log(`Element at index: ${i} , value: ${arr1[i]}`);
+}
+
+for (let i = 0; i < arr2.length; i++) {
+    console.log(`Element at index: ${i} , value: ${arr2[i]}`);
 }
 
 // console.log(arr1[0]);
