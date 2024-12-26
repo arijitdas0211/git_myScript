54b56b04f9d8760c77a0e228603a63d2594143dd
Arijit Das <143489977+arijitdas0211@users.noreply.github.com>
2024-12-26 11:13:01 +0530

    ArrayMethods.js 1

--- ArrayMethods.js
+++ ArrayMethods.js
@@ -15,3 +15,14 @@ for (var i = 0; i < arr.length; i++) {
         console.log(arr[i]);
     }
 }
+
+var arr2 = [1, 2, 3, 5, 26];
+console.log("Original Array: ", arr2);
+
+// Appends element at the end of the array.
+arr2.push(500);
+console.log("After push 500: ", arr2);
+
+// Appends element at the beginning of the array.
+arr2.unshift(7);
+console.log("After unshift 7: ", arr2);
