99277ef463cc4dffb8f6a8c0599a0558c9eeae06
Arijit Das <143489977+arijitdas0211@users.noreply.github.com>
2024-12-26 10:05:33 +0530

    ArrayObject.js

---
+++ ArrayObject.js
@@ -0,0 +1,10 @@
+var arr1 = [10, 20, 30, 40];
+
+for (let i = 0; i < arr1.length; i++) {
+    console.log(`Element at index: ${i} , value: ${arr1[i]}`);
+}
+
+// console.log(arr1[0]);
+// console.log(arr1[1]);
+// console.log(arr1[2]);
+// console.log(arr1[3]);
