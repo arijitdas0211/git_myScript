05c98924606b3e0bf4d354f7b01b17b7ee8dec6c
Arijit Das <143489977+arijitdas0211@users.noreply.github.com>
2024-12-27 13:48:52 +0530

    Array Interation 1

--- ArrayIterationMethod.js
+++ ArrayIterationMethod.js
@@ -1,8 +1,8 @@
 let arr = [10,20,30,40,50,65];
 
-for (let i=0; i<arr.length; i++) {
-    console.log("Index: ",i,"Element: ",arr[i]);
-}
+// for (let i=0; i<arr.length; i++) {
+//     console.log("Index: ",i,"Element: ",arr[i]);
+// }
 
 console.log("\n========================================\n");
 
@@ -10,3 +10,12 @@ console.log("\n========================================\n");
 arr.forEach(function(element, index) {
     console.log(element, index);
 });
+
+console.log("\n========================================\n");
+
+// filter(): 
+let nums = [10, 23, 37, 40, 50, 65];
+let odd = nums.filter((elem) => elem%2 !== 0);
+console.log("Odd Numbers are: ", odd);
+
+// map():
