4999608858f9f23fc478983864bb58d46885c009
Arijit Das <143489977+arijitdas0211@users.noreply.github.com>
2024-12-30 10:48:44 +0530

    Map_reduce

---
+++ Map_Reduce.js
@@ -0,0 +1,22 @@
+var array = [1,2,3,4,5,6];
+
+// var sqArr = array.map(function(elem) {
+//     return elem*elem;
+// })
+
+// map():
+var sqArr = array.map((elem)=>elem*elem);
+
+console.log("Original array: ", array);
+console.log("Squared array: ", sqArr);
+
+
+// The fucntion which is passed as an arguement is called a callback function.
+// Example: Arrow function
+
+
+// reduce():
+let arr = [1,2,3,4];
+
+var sum = arr.reduce((acc, elem)=> acc + elem , 0);
+console.log(sum);
