ba24511442c02a1571a5f5d2eac17d2bdde73add
Arijit Das <143489977+arijitdas0211@users.noreply.github.com>
2025-01-16 11:01:41 +0530

    ExceptionHandling 1

--- ExceptionHandling.js
+++ ExceptionHandling.js
@@ -1,5 +1,19 @@
 /**
  * Compile time errors - Syntax errors
  * Runtime errors - Logical errors
+ * Exception handling is the process where we can handle sudden termination of the code.
  * 
  */
+
+try {
+    // code that might throw an error.
+} catch {
+    // code to handle the error.
+} finally {
+    // Optional block to execute after try or catch.
+}
+
+function add() {
+    console.log("This function will perform add operation");
+}
+subtract(); // This will throw an error.
