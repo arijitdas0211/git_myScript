3c895ee7c3cc016f21ec45c8034d0bf94cda24aa
Arijit Das <143489977+arijitdas0211@users.noreply.github.com>
2025-01-24 00:51:15 +0530

    SideBar 1

--- SideBar/script.js
+++ SideBar/script.js
@@ -4,15 +4,18 @@ const closebtn = document.getElementById("closebtn");
 const sidebar = document.getElementById("sidebar");
 
 button.addEventListener('click', () => {
-    barbtn.style.display = "block";
-    closebtn.style.display = "block";
-    sidebar.style.display = "block";
 
     if (barbtn.style.display === "block") {
         console.log("Barbutton Clicked");
+        barbtn.style.display = "none";
+        closebtn.style.display = "block";
+        sidebar.style.display = "none";
     }
 
-    if (barbtn.style.display === "none") {
+    else {
         console.log("Closebtn Clicked");
+        barbtn.style.display = "block";
+        closebtn.style.display = "none";
+        sidebar.style.display = "block";
     }
 })
